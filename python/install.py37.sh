90e110f745f600f51130f9e09d814244a094ac3e
xx9928203 <33280993+xx9928203@users.noreply.github.com>
2022-08-08 15:02:35 +0800

    Update install.py37.sh

--- python/install.py37.sh
+++ python/install.py37.sh
@@ -6,7 +6,7 @@ yum install -y  libffi-devel
 cd /opt/
 ##wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
 wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz
-tar -xvJf  Python-3.7.0.tar.xz
+tar -xvJf  Python-3.7.4.tar.xz
 
 mkdir -p /usr/local/python3
 cd Python-3.7.4
