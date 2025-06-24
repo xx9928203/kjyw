76d5a706f2bcd4b51888f9e8037cd994a40e0fbf
xx9928203 <33280993+xx9928203@users.noreply.github.com>
2025-06-24 22:08:43 +0800

    Update install_ssl.sh

--- python/install_ssl.sh
+++ python/install_ssl.sh
@@ -5,3 +5,7 @@ cd openssl-1.1.1u
 ./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl shared zlib
 make -j$(nproc)
 sudo make install
+
+echo "usr/local/openssl/lib/" | sudo tee /etc/ld.so.conf.d/openssl-1.1.1u.conf
+sudo ldconfig
+openssl version
